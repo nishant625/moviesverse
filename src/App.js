0edf57c532142b4d7cbb2b32860ac117453e15ae
nishant625 <127782577+nishant625@users.noreply.github.com>
2024-07-03 00:14:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import SearchIcon from './search.svg'
 import MovieCard from './MovieCard';
 //43580899
 
-const API_URL='http://www.omdbapi.com/?i=tt3896198&apikey=43580899';
+const API_URL='https://www.omdbapi.com/?i=tt3896198&apikey=43580899';
 
 
 function App() {
